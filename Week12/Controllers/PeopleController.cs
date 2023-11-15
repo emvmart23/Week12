907ef5ee8c732763602d67d1db2172d4f45f31a3
Alumno <Alumno@LAB1504-27>
2023-11-14 21:16:55 -0500

    Agregar archivos de proyecto.

---
+++ Week12/Controllers/PeopleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        public List<String> Get()
+        {
+            return null;
+        }
+        public string GetById()
+    }
+}
